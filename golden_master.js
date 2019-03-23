95fbd41651227788336aed130eb0a60fa3994c17
Keith Maxwell <keith.maxwell@gmail.com>
2019-03-23 11:34:04 +0000

    Change to use a variable

--- golden_master.js
+++ golden_master.js
@@ -3,19 +3,18 @@ require("./game.js"); /* global Game */
 var notAWinner = false;
 
 var game = new Game();
+var randomNumberGenerator = Math.random;
 
-game.add('Chet');
-game.add('Pat');
-game.add('Sue');
+game.add("Chet");
+game.add("Pat");
+game.add("Sue");
 
-do{
+do {
+  game.roll(Math.floor(randomNumberGenerator() * 6) + 1);
 
-  game.roll(Math.floor(Math.random()*6) + 1);
-
-  if(Math.floor(Math.random()*10) == 7){
+  if (Math.floor(randomNumberGenerator() * 10) == 7) {
     notAWinner = game.wrongAnswer();
-  }else{
+  } else {
     notAWinner = game.wasCorrectlyAnswered();
   }
-
-}while(notAWinner);
+} while (notAWinner);
