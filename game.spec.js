d1bd809ef4130fd4467483add1c490a969236daf
Keith Maxwell <keith.maxwell@gmail.com>
2019-03-23 11:24:03 +0000

    Add tests for the predictable start of the output

--- game.spec.js
+++ game.spec.js
@@ -14,4 +14,23 @@ describe("The test environment", function() {
 
 describe("Your specs...", function() {
   // it ...
+  it("should say 'Chet was added'", function() {
+    console.log = jasmine.createSpy("log");
+    var game = new Game();
+    game.add("Chet");
+    game.add("Pat");
+    game.add("Sue");
+
+    expect(console.log).toHaveBeenCalledWith("Chet was added");
+    expect(console.log).toHaveBeenCalledWith("Pat was added");
+    expect(console.log).toHaveBeenCalledWith("Sue was added");
+    expect(console.log).toHaveBeenCalledWith("They are player number 1");
+    expect(console.log).toHaveBeenCalledWith("They are player number 2");
+    expect(console.log).toHaveBeenCalledWith("They are player number 3");
+
+    game.roll(Math.floor(Math.random() * 6) + 1);
+
+    expect(console.log).toHaveBeenCalledWith("Chet is the current player");
+    expect(console.log).toHaveBeenCalledWith("They have rolled a 3");
+  });
 });
